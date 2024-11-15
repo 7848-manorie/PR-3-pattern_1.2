53a3b1ad904ccad7364a7b9d93ca2e08ce561472
pooja00617 <pooja.rnw@gmail.com>
2024-11-15 18:14:57 +0530

    first commit

---
+++ info.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int age=19;
+    char name[]="manorie",school[]="Lourdes";
+    printf("name:%s",name);
+    printf("\nage:%d",age);
+    printf("\nschool:%s",school);
+}
---
+++ rnw_pattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    printf("---------");
+    printf("\n|       |");
+    printf("\nR       |");
+    printf("\nN       |");
+    printf("\nW       |");
+    printf("\n|       |");
+    printf("\n---------");
+    getch();
+
+}
